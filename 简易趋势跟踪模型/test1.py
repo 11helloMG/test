c331c95d2f8873825458b2b6f75b2e61e45a3f52
Master <Fengrl0510@gmail.com>
2025-08-19 20:15:07 +0800

    删除了测试文件 test1.py

--- 简易趋势跟踪模型/test1.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import efinance as ef
-import matplotlib.pyplot as plt
-
-# 下载中股数据
-stock_code = '002245'
-data = ef.stock.get_quote_history(stock_code)
-print(data.head())
